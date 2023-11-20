f8c00b40299f32f86543fdb54f693a3290916c45
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2023-11-20 10:46:53 -0600

    Add Lazy.nvim package manager
    Adds bootstrapping logic for Lazy
    Adds some tpope plugins
    Preparing for plugin organization later

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,6 +2,39 @@
 ---- User Configs ----------------------------------------------------
 ----------------------------------------------------------------------
 
+--- Remaps must come before plugins or the wrong leader key is used.
 require("kenson.remap")
 require("kenson.options")
 
+----------------------------------------------------------------------
+---- Bootstrap & Run Plugin Manager ----------------------------------
+----------------------------------------------------------------------
+
+--- Bootstrap lazy.nvim plugin manager.
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+--- Setup plugins
+require("lazy").setup({
+  --- This spot is good for plugins that don't require configuration.
+
+  --- Git plugins
+  'tpope/vim-fugitive',
+  'tpope/vim-rhubarb',
+
+  --- Detect tabstop and shiftwidth automagically.
+  'tpope/vim-sleuth',
+
+  --- Compile all of the configurations in the plugins directory.
+---  { import = "plugins" }
+})
