f73aa99a9e599df909fce738b7b086b95833ae55
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2023-11-22 14:05:47 -0600

    Move setting theme to colors.lua

---
+++ nvim/lua/kenson/colors.lua
@@ -0,0 +1,16 @@
+----------------------------------------------------------------------
+---- Theme Configurations --------------------------------------------
+----------------------------------------------------------------------
+
+--- Use Truecolor 24-bit colors in terminal
+vim.opt.termguicolors = true
+
+--- Moonfly
+vim.cmd.colorscheme('moonfly')
+
+--- Gruvbox
+vim.opt.background = 'dark'
+-- vim.g.gruvbox_contrast_dark = 'hard'
+-- vim.g.termcolors = 256
+-- vim.cmd.colorscheme('gruvbox')
+
