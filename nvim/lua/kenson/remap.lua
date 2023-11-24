fc370f64ea43feeda4178da0835d7df1a8368765
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2023-11-23 18:17:34 -0600

    Added no-op key binding

--- nvim/lua/kenson/remap.lua
+++ nvim/lua/kenson/remap.lua
@@ -71,7 +71,10 @@ vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
---- Diagnostic keymaps
+--- Helps with accidently trying a keymap in the wrong mode
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+--- Diagnostic keymaps (from kickstart)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
