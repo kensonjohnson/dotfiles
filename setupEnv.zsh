a8afcb4d05ea05e2e7ce0c7fcc1911917152c37f
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-09-10 16:08:14 -0500

    Fix symlink path

--- setupEnv.zsh
+++ setupEnv.zsh
@@ -5,8 +5,8 @@
 # TODO: Check for proper placement of .dotfiles directory
 # TODO: Add idempotent wraps for each link
 # Symbolic Links
-ln -s ~/.dotfiles/zshrc ~/.config/.zshrc
-ln -s ~/.dotfiles/zshenv ~/.config/.zshenv
+ln -s ~/.dotfiles/zshrc ~/.zshrc
+ln -s ~/.dotfiles/zshenv ~/.zshenv
 # TODO: Add check for .ssh directory, mkdir if not present
 ln -s ~/.dotfiles/ssh/config ~/.ssh/config
 ln -s ~/.dotfiles/nvim ~/.config/nvim
