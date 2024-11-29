76f3f53382d0e35c2e21be9ea9a4e381f113827c
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-11-29 15:26:15 -0600

    Add file tree

---
+++ nvim/lua/plugins/tree.lua
@@ -0,0 +1,20 @@
+return {
+  "nvim-tree/nvim-tree.lua",
+  version = "*",
+  lazy = false,
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    require("nvim-tree").setup {
+      actions = {
+        open_file = {
+          quit_on_open = true
+        },
+      },
+      vim.keymap.set("n", "\\", ":NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }),
+      vim.keymap.set("n", "<leader>pe", ":NvimTreeToggle<CR>", { desc = "Open [P]roject [E]xplorer" }),
+    }
+  end,
+}
+
