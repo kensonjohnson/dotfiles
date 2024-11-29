020f04d84fcee5653f398853d17a04368e414d66
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-11-29 16:53:01 -0600

    Fix whitespace

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -5,23 +5,23 @@ return {
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 
-        'bash', 
-        'diff', 
-        'html', 
-        'lua', 
-        'luadoc', 
-        'markdown', 
-        'markdown_inline', 
-        'query', 
-        'vim', 
-        'vimdoc', 
-        'tsx', 
-        'go', 
-        'python', 
-        'javascript', 
-        'typescript', 
-        'sql' 
+      ensure_installed = {
+        'bash',
+        'diff',
+        'html',
+        'lua',
+        'luadoc',
+        'markdown',
+        'markdown_inline',
+        'query',
+        'vim',
+        'vimdoc',
+        'tsx',
+        'go',
+        'python',
+        'javascript',
+        'typescript',
+        'sql'
       },
       auto_install = true,
       highlight = {
