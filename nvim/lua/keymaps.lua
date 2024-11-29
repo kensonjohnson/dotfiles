b339acc8d189d59f353334457566fc9eae27cbb5
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-11-29 13:39:23 -0600

    base setup

---
+++ nvim/lua/keymaps.lua
@@ -0,0 +1,86 @@
+----------------------------------------------------------------------
+---- Custom Remaps ---------------------------------------------------
+----------------------------------------------------------------------
+
+--- Write buffer to file
+vim.keymap.set('n', '<leader>w', ':w<CR>')
+
+--- Escape INSERT mode
+vim.keymap.set('i', 'jj', '<esc>')
+vim.keymap.set('i', 'jk', '<esc>')
+vim.keymap.set('i', 'kk', '<esc>')
+
+--- Append line while keeping cursor at column 0
+vim.keymap.set("n", "J", "mzJ`z")
+
+--- Keep cursor centered when jumping by page
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+--- Keep cursor centered when jumping by search term
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+--- Quickfix list navigation
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+--- Clear search highlighting
+vim.keymap.set('n', '<Esc>', vim.cmd.nohls)
+
+--- Paste without replacing buffer in VISUAL mode
+vim.keymap.set('x', '<leader>p', [["_dP]])
+
+--- Delete character without replacing buffer
+vim.keymap.set({ 'n', 'x' }, 'x', [["_x]])
+
+--- Yank to system keyboard
+vim.keymap.set({ 'n', 'v' }, '<leader>y', [["+y]])
+
+--- Yank contents of entire file to system clipboard
+vim.keymap.set('n', '<leader>Y', [[gg"+yG]])
+
+--- Globally rename word cursor is on
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+--- Make current file executable
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+--- Move VISUAL highlighted region up or down
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+
+--- Attemp to format current buffer
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, { desc = "Format current buffer" })
+
+-- Remap for dealing with word wrap
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+--- Create new line while in insert mode
+vim.keymap.set('i', '/n', '<C-o>o', { desc = 'Create new line under current line' })
+
+--- Helps with accidently trying a keymap in the wrong mode
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+--- Helpful window navigation commands 
+vim.keymap.set('n', '<Left>', '<C-w>h', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<Right>', '<C-w>l', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<Down>', '<C-w>j', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<Up>', '<C-w>k', { desc = 'Move focus to the upper window' })
+vim.keymap.set('n', '<C-Left>', '<C-w><', { desc = 'Decrease the width of the current window' })
+vim.keymap.set('n', '<C-Right>', '<C-w>>', { desc = 'Increase the width of the current window' })
+vim.keymap.set('n', '<C-Down>', '<C-w>-', { desc = 'Decrease the height of the current window' })
+vim.keymap.set('n', '<C-Left>', '<C-w>+', { desc = 'Increase the height of the current window' })
+
+--- Create interactive terminals
+vim.keymap.set("n", "<leader>t", "<cmd>terminal<CR>i", { desc = "Open [t]erminal in current window" })
+vim.keymap.set("n", "<leader>ts", "<cmd>split | terminal<CR>i", { desc = "Open [t]erminal in a [s]plit window" })
+vim.keymap.set("n", "<leader>tv", "<cmd>vsplit | terminal<CR>i", { desc = "Open [t]erminal in a [v]ertical split window" })
+
+--- :!just keymaps
+vim.keymap.set('n', '<leader>jr', '<cmd>split | terminal just run<CR>i', { desc = '[j]ust [r]un' })
+vim.keymap.set('n', '<leader>jd', '<cmd>split | terminal just dev<CR>i', { desc = '[j]ust [d]ev' })
+vim.keymap.set('n', '<leader>jl', '<cmd>!just list<CR>', { desc = '[j]ust [l]ist' })
---
+++ nvim/lua/options.lua
@@ -0,0 +1,63 @@
+----------------------------------------------------------------------
+---- Editor Configurations -------------------------------------------
+----------------------------------------------------------------------
+
+--- Use Truecolor 24-bit colors in terminal
+vim.opt.termguicolors = true
+
+--- Turn on syntax for any theme
+vim.opt.syntax = 'on'
+
+--- Add relative line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+--- Sets distance from bottom of screen before auto-scrolling
+vim.opt.scrolloff = 10
+
+--- Ignores uppercase when searching unless uppercase is in search term
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+--- Allow line wrap to virtual lines
+vim.opt.wrap = true
+
+--- Preserve indentation on virtual lines
+vim.opt.breakindent = true
+
+--- Set tabstop and indentation preferences
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+--- Use persistent undo
+vim.opt.undofile = true
+
+--- Decrease update time
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 250
+
+--- Better completion experience
+vim.opt.completeopt = 'menuone,noselect'
+
+--- signcolumn by default
+vim.opt.signcolumn = 'yes'
+
+--- Add column at 80 characters
+vim.opt.colorcolumn = '80'
+
+--- Open splits on the bottom
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+--- Highlight on yank
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
