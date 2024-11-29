10304666ef4d3f26bc34ea381efe872ea5d409e1
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-11-29 15:56:57 -0600

    Extract loose plugins to a misc file

--- nvim/lua/lazy-setup.lua
+++ nvim/lua/lazy-setup.lua
@@ -8,18 +8,8 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
+--- Load all plugins defined in 'lua/plugins'
 require("lazy").setup({
-  --- This is a good place for plugins that don't require configuration.
-
-  --- Detect tabstop and shiftwidth automagically
-  'tpope/vim-sleuth',
-
-  --- <leader>gc to comment visually selected regions
-  'numToStr/Comment.nvim',
-
-  --- Create closing brackets automagically
-  'm4xshen/autoclose.nvim', 
-
-  --- Load all plugins defined in 'lua/plugins'
   { import = "plugins" },
 },{})
+
---
+++ nvim/lua/plugins/misc.lua
@@ -0,0 +1,28 @@
+return {
+  --- Detect tabstop and shiftwidth automagically
+  'tpope/vim-sleuth',
+
+  --- <leader>gc to comment visually selected regions
+  { 'numToStr/Comment.nvim',  opts = {} },
+
+  --- Create closing brackets automagically
+  { 'm4xshen/autoclose.nvim', opts = {} },
+
+  --- Allow the use of <ESC> to leave built-in terminal
+  {
+    "sychen52/smart-term-esc.nvim",
+    config = function()
+      require("smart-term-esc").setup {
+        key = "<Esc>",
+        except = { "nvim", "fzf" }
+      }
+    end
+  },
+
+  --- Keep the cursor from reaching the bottom of the buffer
+  {
+    'Aasim-A/scrollEOF.nvim',
+    event = { 'CursorMoved', 'WinScrolled' },
+    opts = {},
+  },
+}
