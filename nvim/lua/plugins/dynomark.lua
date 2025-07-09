8d04ce86b56fd75ad4765bade514987535050da2
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2025-07-09 15:11:03 -0500

    Start dynomark plugin upon entering first markdown buffer for session

--- nvim/lua/plugins/dynomark.lua
+++ nvim/lua/plugins/dynomark.lua
@@ -1,9 +1,29 @@
 return {
 	"k-lar/dynomark.nvim",
 	dependencies = "nvim-treesitter/nvim-treesitter",
+	ft = "markdown",
 	opts = {
 		results_view_location = "horizontal",
 	},
+	config = function(_, opts)
+		require("dynomark").setup(opts)
+
+		-- Track if Dynomark has been globally enabled
+		local dynomark_enabled = false
+
+		-- Auto-enable for first markdown file opened
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "markdown",
+			callback = function()
+				if not dynomark_enabled then
+					dynomark_enabled = true
+					vim.schedule(function()
+						vim.cmd("Dynomark toggle")
+					end)
+				end
+			end,
+		})
+	end,
 	keys = {
 		{
 			"<leader>v",
