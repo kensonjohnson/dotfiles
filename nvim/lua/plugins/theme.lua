3d6676099fe16369cc8b473b1c360a8b7617218d
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2025-07-22 09:00:44 -0500

    Change spell check color

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -3,5 +3,10 @@ return {
 	priority = 1000,
 	init = function()
 		vim.cmd.colorscheme("kanagawa-dragon")
+		--- Change spellchecker underline color
+		vim.api.nvim_set_hl(0, "SpellBad", { undercurl = true, sp = "CornflowerBlue" })
+		vim.api.nvim_set_hl(0, "SpellCap", { undercurl = true, sp = "CornflowerBlue" })
+		vim.api.nvim_set_hl(0, "SpellRare", { undercurl = true, sp = "CornflowerBlue" })
+		vim.api.nvim_set_hl(0, "SpellLocal", { undercurl = true, sp = "CornflowerBlue" })
 	end,
 }
